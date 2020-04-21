85aadace1c34f6c72ad7b5fe9b857fbb45322e9c
Ryan <ryanhansen2222@gmail.com>
2020-04-20 22:23:11 -0600

    Files are 0/1

--- code/preprocessing/handledata.py
+++ code/preprocessing/handledata.py
@@ -47,19 +47,19 @@ if __name__ == '__main__':
     '''
 
 
-    #print(corona_data_frame)
+    print(corona_data_frame)
 
      #turn the data into 1's (if number is greater than 50) or a 0 (if number is less than 50)
     for column_labels, column_values in corona_data_frame.items():
         row_num = 0
-        #print(column_labels)
-        if column_labels != 'Province/State' and 'Country/Region' and 'Lat' and 'Long':
+        if (column_labels != 'Province/State') and (column_labels !='Country/Region') and (column_labels !='Lat') and (column_labels !='Long'):
             for col_val in column_values:
-                #print(col_val)
-                if col_val <= 50:
-                    corona_data_frame.loc[row_num, column_labels] = 0
-                elif col_val > 50:
-                    corona_data_frame.loc[row_num, column_labels] = 1
+                x = corona_data_frame.loc[row_num+1, column_labels]
+
+                if int(col_val) <= 50:
+                    corona_data_frame.loc[row_num+1, column_labels] = 0
+                else:
+                    corona_data_frame.loc[row_num+1, column_labels] = 1
                 row_num = row_num + 1
     
     print(corona_data_frame)
